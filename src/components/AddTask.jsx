1fddfe92ea5c65eb795ab10ece9c9f8c51111e60
RaselMridha792 <raselmridha792@gmail.com>
2025-02-21 05:20:28 +0600

    creating add task popup

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,97 @@
+import { useContext, useState } from "react";
+import { CiSquareChevDown } from "react-icons/ci";
+
+const AddTask = ({user}) => {
+      const [taskName, setTaskName] = useState("")
+      const [descriptionName, setDescriptionName] = useState("")
+      const [error1, setError1] = useState("");
+      const [error2, setError2] = useState("");
+
+      const handleTaskNameLimit = (e) =>{
+
+            const value = e.target.value;
+
+            if(value.length > 50){
+                  setError1('Task name cannot exceed 50 caracters');
+            }else{
+                  setError1("");
+            }
+            setTaskName(value);
+      }
+      const handleDescriptionLimit = (e) =>{
+
+            const value = e.target.value;
+
+            if(value.length > 200){
+                  setError2('Description cannot exceed 200 caracters');
+            }else{
+                  setError2("");
+            }
+            setDescriptionName(value);
+      }
+
+      const handleAddTask = (e) =>{
+            e.preventDefault()
+            const email = user.email;
+            const form = e.target;
+            const taskName = form.taskName.value;
+            const description = form.description.value;
+            const category = form.category.value
+            console.log({taskName, description, category, email});
+      }
+  return (
+    <>
+      <div className="max-w-screen-2xl mx-auto">
+        <h1 className="text-center font-bold capitalize text-2xl">
+          Add New task
+        </h1>
+        <div className="divider"></div>
+        <form onSubmit={handleAddTask} className="space-y-5">
+          <div className="w-full flex flex-col gap-1">
+            <label>
+              <span className="text-xl fond-bold">Task Name <span className="text-sm">- max 50 characters</span></span>
+            </label>
+            <input
+              type="text"
+              name="taskName"
+              onChange={handleTaskNameLimit}
+              value={taskName}
+              maxLength={51}
+              placeholder="Enter Task Name"
+              className="input input-bordered"
+              id=""
+            />
+            {error1 && <span className="text-red-500">{error1}</span>}
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label>
+              <span className="text-xl fond-bold">Task Description  <span className="text-sm"> - max 200 characters</span></span>
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              value={descriptionName}
+              onChange={handleDescriptionLimit}
+              maxLength={201}
+              placeholder="Enter Task Name"
+              className="input input-bordered h-40"
+              id=""
+            />
+            {error2 && <span className="text-red-500">{error2}</span>}
+          </div>
+          <div>
+            <span>select category: </span>
+            <select className="border p-3 rounded-xl" required name="category" id="">
+                  <option value="to do" selected>To Do</option>
+                  <option value="pending">Pending</option>
+                  <option value="done">Done</option>
+            </select>
+          </div>
+          <input type="submit" value="Add Task" className="btn btn-neutral" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddTask;
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,7 +1,32 @@
+import { FaEdit } from "react-icons/fa";
+import AddTask from "../../components/AddTask";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
+
 const Homepage = () => {
+  const {user} = useContext(AuthContext)
   return (
     <>
-      <h1>this is home</h1>
+      <div className="max-w-screen-2xl mx-auto px-5">
+        <div className="my-5 flex items-center justify-center">
+          <button onClick={() => document.getElementById("my_modal_4").showModal()} className="btn btn-neutral">
+            <FaEdit></FaEdit> Add New Task
+          </button>
+        </div>
+        <dialog id="my_modal_4" className="modal">
+          <div className="modal-box w-11/12 max-w-5xl">
+          <AddTask user={user}></AddTask>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button, it will close the modal */}
+                <button className="btn">Add Task</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        <div className="divider"></div>
+        <div></div>
+      </div>
     </>
   );
 };
