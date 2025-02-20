4d63a9879fccbadcd84314ab238c1ec213bcc28a
RaselMridha792 <raselmridha792@gmail.com>
2025-02-21 02:07:23 +0600

    create observer for user state save

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import auth from "../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
 export const AuthContext = createContext();
@@ -7,23 +7,39 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
       const [changeColor, setChangeColor] = useState(true);
       const provider = new GoogleAuthProvider();
+      const [user, setUser] = useState(null);
+      const [loader, setLoader] = useState(false);
 
 
       const RegisterUser = (email, password) =>{
+        setLoader(true)
         return createUserWithEmailAndPassword(auth, email, password)
       }
 
       const LoginUser = (email, password) =>{
+        setLoader(true)
         return signInWithEmailAndPassword(auth, email, password)
       }
 
       const LoginWithGoogle = () =>{
+        setLoader(true)
         return signInWithPopup(auth, provider)
       }
 
 
       useEffect(()=>{
-        const unSubscribe
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+          if(currentUser){
+            setUser(currentUser);
+            setLoader(false);
+          }else{
+            setUser(null);
+            setLoader(true);
+          }
+        });
+        return ()=>{
+          unSubscribe();
+        }
       })
 
 
@@ -34,6 +50,8 @@ const AuthProvider = ({ children }) => {
       RegisterUser,
       LoginUser,
       LoginWithGoogle,
+      loader,
+      user,
   };
   return (
     <>
